8cb1b3d5c9429e9eded0e073983f4f939d34133d
Stephan van Eijkelenburg <stephanvane@gmail.com>
2014-04-15 23:12:17 +0200

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,29 +36,28 @@ set :rbenv_ruby, '2.1.1'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+# namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       # Your restart mechanism here, for example:
+#       # execute :touch, release_path.join('tmp/restart.txt')
+#     end
+#   end
 
-  after :publishing, :restart
+#   after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
 
-end
-
+# end
 
 namespace :clockwork do
   desc 'Start clockwork'
@@ -94,3 +93,7 @@ namespace :clockwork do
     end
   end
 end
+
+after 'puma:start', 'clockwork:start'
+after 'puma:stop', 'clockwork:stop'
+after 'puma:restart', 'clockwork:restart'
