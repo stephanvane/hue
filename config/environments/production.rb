6c57e3fc54f2bfbdeb87cfbbd2f5795071fa3a08
Stephan van Eijkelenburg <stephanvane@gmail.com>
2014-05-26 23:52:57 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hue_api_endpoint = 'http://77.248.22.140:9999/api/stephanvane'
 end
